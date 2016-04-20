486ec42b91f658b15397cb0dd1e548811c337def
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 18:10:43 -0700

    Adding files

---
+++ nice.py
@@ -0,0 +1,75 @@
+from random import choice
+
+from flask import Flask, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return "<a href='./hello'>Hi! This is</a> the home page."
+
+
+@app.route('/hello')
+def say_hello():
+
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          <label>What's your name? <input type="text" name="person"></label>
+          <select name="compliment">
+              <option value="great">Great</option>
+              <option value="nice">Nice</option>  
+              <option value="sweet">Sweet</option>                                        
+          </select>
+          <input type="submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    compliment = request.args.get("compliment")
+    # compliment = choice("compliment")
+    #print boat
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi %s I think you're %s!
+      </body>
+    </html>
+    """ % (player, compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
+    #app.run()
